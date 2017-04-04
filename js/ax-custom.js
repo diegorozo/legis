1c9815f6bc62273be09677371e392fb0c7cb8360
Diego Rozo <rozdiego@gmail.com>
2017-04-04 16:28:34 -0500

    Modulo actualización

--- js/ax-custom.js
+++ js/ax-custom.js
@@ -8,65 +8,150 @@ $(function() {
     });
 });
 
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
+//Modulo Mercado
 
 $('#ax-mercado').click(function(){
-	$('.ax-itemmod').addClass('animated fadeInRight');
-	$('.ax-item-mod').addClass ('animated fadeInRight');
-	$('.ax-itemmod').addClass('ver');
-	$('.ax-item-mod').addClass('ver');
-	$('#ax-mod-img').removeClass();
-	$('#ax-mod-img').addClass('ax-mod-img2 animated bounceInUp');
-	$('#ax-mod-compl2').removeClass();
-	$('#ax-mod-compl2').addClass('col-md-9 ax-md animated fadeIn');
-	$('#ax-mod-compl3').addClass('ver animated fadeIn');
+	$('#ax-mod-estadistica, #ax-mod-ranking, #ax-mod-comercial, #ax-mod-financiero').removeClass();
+	$('#ax-mod-estadistica, #ax-mod-ranking, #ax-mod-comercial, #ax-mod-financiero').addClass('ocultar');	
+	$('#ax-mod-compl2').addClass('ocultar');
+	$('#ax-mod-mercado').removeClass();
+	$('#ax-mod-mercado').addClass('ver');
+	$('#ax-mod-mercado').addClass('col-md-9 animated fadeIn ver ax-md');
+	$('#ax-mod-mercado .ax-md-2').addClass('ver');
+	$('#ax-mod-mercadoico').addClass('ver');
+
 	
   return false;
 
 });
 
-//
-//$('#ax-mod-img').click(function(){
-//	$('#ax-mod-img').removeClass();
-//	$('#ax-mod-compl4').addClass('ocultar');
-//	
-//	$('#ax-mod-compl2').removeClass();
-//	$('#ax-mod-compl2').addClass('col-md-10 ax-md-2');
-//	
-//	$('#ax-itemmod').removeClass();
-//	$('#ax-itemmod').addClass('ocultar');
-//	
-//	$('#ax-item-mod').removeClass();
-//	$('#ax-item-mod').addClass('ocultar');
-//	
-//	$('#ax-mod-compl3').removeClass();
-//	$('#ax-mod-compl3').addClass('col-md-1 hidden-xs ax-modcomple');
-//	
-//  return false;
-//
-//});
 
-
-$('#ax-mod-compl3').click(function(){
-	$('#ax-mod-compl2').removeClass();
-	$('#ax-mod-compl2').addClass('col-md-6 ax-md opacity');
-	$('#ax-mod-compl3').removeClass();
-	$('#ax-mod-compl3').addClass('ocultar');
-	$('#ax-mod-compl4').removeClass();
-	$('#ax-mod-compl4').addClass('ver col-md-4 ax-modcomple2 animated fadeIn');
+$('#ax-mod-mercadoico').click(function(){
+	$('#ax-mod-mercado').removeClass();
+	$('#ax-mod-mercado').addClass('ver');
+	$('#ax-mod-mercado').addClass('col-md-6 ax-md opacity');
+	$('#ax-mod-mercadoico').removeClass();
+	$('#ax-mod-mercadoico').addClass('ocultar');
+	$('#ax-mod-mercadocont').removeClass();
+	$('#ax-mod-mercadocont').addClass('ver col-md-4 ax-modcomple2 animated fadeIn');
 	
   return false;
 });
 
-$('#ax-mod-compl4').click(function(){
-	$('#ax-mod-compl4').removeClass();
-	$('#ax-mod-compl4').addClass('ocultar');
-	$('#ax-mod-compl2').removeClass();
-	$('#ax-mod-compl2').addClass('col-md-9 ax-md');
-	$('#ax-mod-compl3').removeClass();
-	$('#ax-mod-compl3').addClass('col-md-1 hidden-xs ax-modcomple ver animated fadeIn');
+$('#ax-mod-mercadocont').click(function(){
+	$('#ax-mod-mercadocont').removeClass();
+	$('#ax-mod-mercadocont').addClass('ocultar');
+	$('#ax-mod-mercado').removeClass();
+	$('#ax-mod-mercado').addClass('ver');
+	$('#ax-mod-mercado').addClass('col-md-9 ax-md');
+	$('#ax-mod-mercadoico').removeClass();
+	$('#ax-mod-mercadoico').addClass('col-md-1 hidden-xs ax-modcomple ver animated fadeIn');
 
   return false;
 });
+
+
+//Modulo Mercado
+
+
+
+
+
+
+//Modulo Estadisticos
+
+
+$('#ax-estadistica').click(function(){
+	$('#ax-mod-mercado, #ax-mod-ranking, #ax-mod-comercial, #ax-mod-financiero').removeClass();
+	$('#ax-mod-mercado, #ax-mod-ranking, #ax-mod-comercial, #ax-mod-financiero').addClass('ocultar');
+	$('#ax-mod-compl2').addClass('ocultar');
+	$('#ax-mod-estadistica').removeClass();
+	$('#ax-mod-estadistica').addClass('ver');
+	$('#ax-mod-estadistica').addClass('col-md-9 animated fadeIn ver ax-md');
+	$('#ax-mod-estidisticoico').addClass('ver');
+
+	
+  return false;
+
+});
+
+$('#ax-mod-estidisticoico').click(function(){
+	$('#ax-mod-estadistica').removeClass();
+	$('#ax-mod-estadistica').addClass('ver');
+	$('#ax-mod-estadistica').addClass('col-md-6 ax-md opacity');
+	$('#ax-mod-estidisticoico').removeClass();
+	$('#ax-mod-estidisticoico').addClass('ocultar');
+	$('#ax-mod-estidisticocont').removeClass();
+	$('#ax-mod-estidisticocont').addClass('ver col-md-4 ax-modcomple2 animated fadeIn');
+	
+  return false;
+});
+
+$('#ax-mod-estidisticocont').click(function(){
+	$('#ax-mod-estidisticocont').removeClass();
+	$('#ax-mod-estidisticocont').addClass('ocultar');
+	$('#ax-mod-estadistica').removeClass();
+	$('#ax-mod-estadistica').addClass('ver');
+	$('#ax-mod-estadistica').addClass('col-md-9 ax-md');
+	$('#ax-mod-estidisticoico').removeClass();
+	$('#ax-mod-estidisticoico').addClass('col-md-1 hidden-xs ax-modcomple ver animated fadeIn');
+
+  return false;
+});
+
+//Modulo Estadisticos
+
+
+
+
+
+
+
+
+
+
+
+
+$('#ax-ranking').click(function(){
+	$('#ax-mod-mercado, #ax-mod-estadistica, #ax-mod-comercial, #ax-mod-financiero').removeClass();
+	$('#ax-mod-mercado, #ax-mod-estadistica, #ax-mod-comercial, #ax-mod-financiero').addClass('ocultar');
+	$('#ax-mod-compl2').addClass('ocultar');
+	$('#ax-mod-ranking').removeClass();
+	$('#ax-mod-ranking').addClass('ver');
+	$('#ax-mod-ranking').addClass('col-md-9 animated fadeIn ver ax-md');
+	$('#ax-mod-compl3').addClass('ver');
+
+	
+  return false;
+
+});
+
+$('#ax-comercial').click(function(){
+	$('#ax-mod-mercado, #ax-mod-estadistica, #ax-mod-ranking, #ax-mod-financiero').removeClass();
+	$('#ax-mod-mercado, #ax-mod-estadistica, #ax-mod-ranking, #ax-mod-financiero').addClass('ocultar');
+	$('#ax-mod-compl2').addClass('ocultar');
+	$('#ax-mod-comercial').removeClass();
+	$('#ax-mod-comercial').addClass('ver');
+	$('#ax-mod-comercial').addClass('col-md-9 animated fadeIn ver ax-md');
+	$('#ax-mod-compl3').addClass('ver');
+
+	
+  return false;
+
+});
+
+$('#ax-financiero').click(function(){
+	$('#ax-mod-mercado, #ax-mod-estadistica, #ax-mod-ranking, #ax-mod-comercial').removeClass();
+	$('#ax-mod-mercado, #ax-mod-estadistica, #ax-mod-ranking, #ax-mod-comercial').addClass('ocultar');
+	$('#ax-mod-compl2').addClass('ocultar');
+	$('#ax-mod-financiero').removeClass();
+	$('#ax-mod-financiero').addClass('ver');
+	$('#ax-mod-financiero').addClass('col-md-9 animated fadeIn ver ax-md');
+	$('#ax-mod-compl3').addClass('ver');
+
+	
+  return false;
+
+});
+
+
